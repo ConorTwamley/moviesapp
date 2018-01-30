fea5b808ff993b625e65a9b3de147f4923fcf094
Conor Twamley <ctwamley@gmail.com>
2018-01-30 15:09:07 +0000

    Version 2 added validations

--- Gemfile
+++ Gemfile
@@ -51,3 +51,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem'coffee-script-source','1.8.0'
 
+gem'will_paginate','~>3.1'
+
+
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,14 @@
 class Movie < ApplicationRecord
 	belongs_to :genre
+	validates :title, presence: true
+	validates :title, length:{in: 2..20 }
+	validates :title, uniqueness: true
+	validate :must_have_valid_trailer
+	
+	def must_have_valid_trailer
+		unless trailer.include?("<iframe")
+			errors.add(:trailer, "Must include an iframe tag")
+		end
+	end
+	
 end
